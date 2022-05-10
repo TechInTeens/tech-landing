d7bc026a8efda038d040c0936482ac0e631f613a
CodyAaTherf <aatherfgamerplayzs@gmail.com>
2022-05-10 12:14:44 +0530

    Added Links

---
+++ tch-landing/pages/links/about.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>Know more about TechInTeens</div>
+  )
+}
+
+export default about
---
+++ tch-landing/pages/links/learn/learn.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const learn = () => {
+  return (
+    <div>Learn Different Programming Languages!</div>
+  )
+}
+
+export default learn
